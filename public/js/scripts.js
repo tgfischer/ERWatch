b407e92bb9327d2fee9bd9e67cd310577a3b125b
Tom Fischer <tgfischer6@gmail.com>
2016-09-17 21:58:31 -0400

    Fixed navbar for mobile

---
+++ public/js/scripts.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("nav .button-collapse").sideNav();
+});
--- routes/visit.js
+++ routes/visit.js
@@ -1,34 +1,44 @@
 var express = require('express');
 var Utils = require('../utils/Utils');
+var Visit = require('../models/visit');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/:code', function(req, res, next) {
-  Utils.getEstimatedWaitTime(req.params.code, req.user.hospital, function(err, result) {
+  Visit.findOne({ code: req.params.code }, function(err, visit) {
     if (err) {
       console.log(err);
       res.send({ err: err })
     }
 
-    if (result.visit) {
-      var expectedDate = new Date();
-      expectedDate = new Date(expectedDate.getTime() + result.waitTime * 60000).toLocaleTimeString('en-US', {
-        hour: "2-digit",
-        minute: "2-digit"
-      });
+    // console.log(JSON.stringify(hospital, null,));
 
-      return res.render('visit', {
-        patient: result.visit.patient,
-        waitTime: result.waitTime,
-        visit: result.visit,
-        condition: result.visit.condition,
-        expectedDate: expectedDate
-      });
-    } else {
-      return res.render('visit', {
-        msg: 'This visitor has already been seen by a doctor'
-      });
-    }
+    Utils.getEstimatedWaitTime(req.params.code, visit.hospital, function(err, result) {
+      if (err) {
+        console.log(err);
+        res.send({ err: err })
+      }
+
+      if (result.visit) {
+        var expectedDate = new Date();
+        expectedDate = new Date(expectedDate.getTime() + result.waitTime * 60000).toLocaleTimeString('en-US', {
+          hour: "2-digit",
+          minute: "2-digit"
+        });
+
+        return res.render('visit', {
+          patient: result.visit.patient,
+          waitTime: result.waitTime,
+          visit: result.visit,
+          condition: result.visit.condition,
+          expectedDate: expectedDate
+        });
+      } else {
+        return res.render('visit', {
+          msg: 'This visitor has already been seen by a doctor'
+        });
+      }
+    });
   });
 });
 
